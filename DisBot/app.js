8384853baf1c8bd4af43db04c8dd7458b17e60a5
KoreusZ <thekoreus.z@gmail.com>
2018-01-10 17:23:18 -0500

    More work towards configurability

--- DisBot/app.js
+++ DisBot/app.js
@@ -2,28 +2,61 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const settings = require('./settings.json')
 const interjections = require('./interject.json')
-const commands = require ('./cmd.json')
-if (settings.token === ''){
-	console.log('You must add a valid bot token to settings.json')
+const commands = require('./cmd.json')
+if (settings.token === '') {
+    console.log('You must add a valid bot token to settings.json');
 }
 client.login(settings.token)
-client.on('ready' ,() => {
-	console.log(settings.motd);
+client.on('ready', () => {
+    console.log(settings.motd);
 });
-client.on('message', message => {
-    var prefix = "/"
-    if message.content.startsWith(prefix)) return;
-    var args = message.content.split(' ').slice(1)
-    var argresult = args.join(' ');
-    //check if message is command or not
-    if (iscommand(message)) {
-        //check if hard command, if not, then run check commands.json
+
+function Command(message) {
+    if (message.startsWith(settings.hardPrefix)) {
+        //isolate args
+        prefix = settings.hardPrefix;
+        var args = message.content.split(' ').slice(1);
+        var argresult = args.join(' ');
+
+        //hard commands
+        //toDo: require role to use commmands
+        if (message.content === prefix + 'setgame') {
+            client.user.setGame(argresult);
+            return;
+        } else if (message.content === prefix + 'setstatus') {
+            client.user.setStatus(argresult);
+            return;
+        }
+    } else if (message.startsWith(settings.softPrefix)) {
+        //todo: this entire feature
+        return;
+    } else {
+        throw 'Command function was passed a non command';
+        return;
     }
-    //check if message is on interject list
-    else if (){
-        //random to interject based on probability according to interject.json
-        if (){
-            //interject
+    return;
+}
+});
+
+client.on('message', message => {
+    //Do not reply to bots
+    if (message.author.bot) {
+        return;
+    };
+
+    //reroute commands to the appropriate function
+    if (message.startsWith(settings.hardPrefix) || message.startsWith(
+            settings.softPrefix) Command(message);
+        return;
+    }
+    else {
+        //check if message is on interject list
+        reply = interject.triggers.findIndex(message);
+        if (reply === -1) {
+            return;
+        } else {
+            client.user.send(interject.replies[reply]);
+            return;
         }
     }
 }
